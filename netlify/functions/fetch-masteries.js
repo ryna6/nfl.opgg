137ffad7fbcacb252d04f13ba493b8657f1d394c
ryna6 <ryanjw629.h@gmail.com>
2025-07-23 18:37:42 +0800

    Update fetch-masteries.js

--- netlify/functions/fetch-masteries.js
+++ netlify/functions/fetch-masteries.js
@@ -1,11 +1,2 @@
-const fetch = require('node-fetch');
-exports.handler = async (event) => {
-  const { puuid } = event.queryStringParameters;
-  const API_KEY = process.env.RIOT_API_KEY;
-  // choose platform-route (e.g. `na1.api.riotgames.com`)
-  const host = 'na1.api.riotgames.com';
-  const url = `https://${host}/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuid}/top`;
-  const res = await fetch(url, { headers: { 'X-Riot-Token': API_KEY } });
-  const data = await res.json();
-  return { statusCode: res.status, body: JSON.stringify(data) };
-};
+GET /.netlify/functions/fetch-masteries?puuid=<player-puuid>
+→ Riot’s /lol/champion-mastery/v4/champion-masteries/by-puuid/{puuid}/top
