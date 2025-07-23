e1757d278256b2f2c8938f665aae5cdbf901d298
ryna6 <ryanjw629.h@gmail.com>
2025-07-23 18:38:18 +0800

    Update fetch-timeline.js

--- netlify/functions/fetch-timeline.js
+++ netlify/functions/fetch-timeline.js
@@ -1,9 +1,2 @@
-const fetch = require('node-fetch');
-exports.handler = async (event) => {
-  const { matchId } = event.queryStringParameters;
-  const API_KEY = process.env.RIOT_API_KEY;
-  const url = `https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}/timeline`;
-  const res = await fetch(url, { headers: { 'X-Riot-Token': API_KEY } });
-  const data = await res.json();
-  return { statusCode: res.status, body: JSON.stringify(data) };
-};
+GET /.netlify/functions/fetch-timeline?matchId=<match-id>
+→ Riot’s /lol/match/v5/matches/{matchId}/timeline
